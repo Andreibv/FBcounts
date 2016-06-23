e9a524ec60faa0a8f455dbcfb176e1097e17bdc3
Andrei B <brcandrey@hotmail.com>
2016-06-23 10:20:22 -0600

    Testing push

---
+++ FBcounts.php
@@ -0,0 +1,9 @@
+<?php
+//para buscar el id de alguna pagina: http://findmyfacebookid.com/
+$json = file_get_contents('http://api.facebook.com/method/fql.query?format=json&query=select+fan_count,name+from+page+where+page_id%3D113462782644');
+
+$decode = json_decode($json);
+
+echo $decode[0]->fan_count."<br>";
+echo $decode[0]->name;
+?>
